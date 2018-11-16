148ed9ba1a694a60dba1399df8266417309e71aa
Joseph G. Varilla <jvarilla981@gmail.com>
2018-11-16 12:14:51 -0600

    Add

--- src/core/config.js
+++ src/core/config.js
@@ -5,6 +5,6 @@ export default {
     jwt: process.env.JWT || 'stubJWT'
   },
   api: {
-    base_url: process.env.BASE_URL || 'https://conduit.productionready.io/api'
+    base_url: process.env.BASE_URL || 'https://powerful-lake-14918.herokuapp.com/api'
   }
 };
